3c2cc7c45c59a6060c716247384274fe71382e5b
Leo1G2 <leo.gruel@ynov.com>
2024-07-07 15:17:24 +0200

    fin de game.js

--- general/game.js
+++ general/game.js
@@ -6,8 +6,43 @@ async function game(players) {
         console.log('Lancer' +(i+1)+'');
         for (const player of players) {
             console.log('C est au tour de ' +(player.name)+':');
-            const
-        }
+            const { premierLancer } = await inquirer.prompt ([
+                {
+                type: 'input',
+                name: 'premierLancer',
+                message: ' Combien de quilles au premier lancer',
+                validate: function(value) {
+                    const valid = !isNaN(value) && value >= 0 && value <= 10;
+                    if (valid) {
+                        return true;
+                    }
+                    return 'Entrez de nouveau le nombre de quilles';
+                },
+                filter: Number
+                }
 
+            ]);
+            player.frames[i].lancers.push(premierLancer);
+            if (firstRoll <10) {
+                const quillesRestantes= 10-premierLancer;
+                const { deuxiemeLancer} = await inquirer.prompt([
+                    {
+                        type: 'inpu',
+                        name: 'deuxiemeLancer',
+                        message: 'Combien de quilles au deuxieme lancer',
+                        validate: function(value) {
+                            const valid = !isNaN(value) && value >=0 && value < quillesRestantes; 
+                            if (valid) {
+                                return true;
+                            }
+                            return 'Entrez de nouveau le nombre de quilles'
+                        },
+                        filter: Number
+                    }
+                ]);
+                player.frames[i].lancers.push(deuuxiemeLancer);
+            }
+            }
     }
 }
+export {game};
--- general/main.js
+++ general/main.js
@@ -1,14 +1,14 @@
 import console from 'console' ;
 import { getNumberOfPlayers } from './getNumberOfPlayers.js';
 import { getNames } from './getNames.js';
-
+import { game } from './game.js';
 
 
 
 async function main() {
     const numberOfPlayers = await getNumberOfPlayers();
     const players = await getNames(numberOfPlayers);
-    await playGame(players);
+    await game(players);
     calculScore(players);
     showResults(players);
     saveHistory(players);
